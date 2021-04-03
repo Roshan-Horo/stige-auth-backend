ccc572c5672cd2ffa6946a7075fc92fc6b119a93
Roshan-Horo <35597955+Roshan-Horo@users.noreply.github.com>
2021-04-03 16:24:56 +0530

    testing routes add

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,18 +3,25 @@ const router = express.Router()
 import { registerUser, authUser} from '../controller/signupController.js'
 //import { signup} from '../controller/authController.js'
 
-// @desc    - SIGNUP
+// @desc    - testing
 // @route   - GET /api/auth
+//@access   - public
+router.get('/', (req,res) => {
+    res.send('test auth route')
+})
+
+// @desc    - SIGNUP
+// @route   - POST /api/auth
 //@access   - public
 router.post('/', registerUser)
 
 // @desc    - LOGIN
-// @route   - GET /api/auth/login
+// @route   - POST /api/auth/login
 //@access   - public
 router.post('/login', authUser)
 
 // @desc    - Google LOGIN
-// @route   - GET /api/auth/googlelogin
+// @route   - POST /api/auth/googlelogin
 //@access   - public
 
 //router.post('/googlelogin', )
